7c321bf502a7b6b4a1f4baa8bd63c7f594c86c16
shinyooon <talkyfull@gmail.com>
2015-12-12 11:14:50 +0900

    [angular] 3주차

---
+++ public/js/hello.js
@@ -0,0 +1,41 @@
+//namespace 패턴
+/*function helloController(){
+
+}
+이렇게 사용하지 않는 이유 :
+*/
+
+angular.module('hello',[])
+	.controller('HelloController', function($scope, $filter){
+		$scope.hello = {
+			msg: 'hello'
+		}
+
+		$scope.toUpper =function(){
+			$scope.hello.msg = $filter('uppercase')($scope.hello.msg);
+		}
+		//$scipe.helllo.msg = '안녕하세요' 동일하지만, 위의 객체형태 선언 방식이 추가하기가 편하다.
+		$scope.delete =function(index){
+			//console.log($scope.items[index]);
+			$scope.items.splice(index, 1);
+		}
+
+		$scope.items = [
+		{
+			title : '볼펜',
+			count : 4,
+			price : 1800
+		},
+		{
+			title : '지우개',
+			count : 1,
+			price : 800
+		}
+		,{
+			title : '연필',
+			count : 12,
+			price : 400
+		}
+		]
+
+	});
