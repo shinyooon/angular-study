dd920fa3c93dae67b8ec7b0a2b3f421416c935d7
shinyooon <talkyfull@gmail.com>
2016-01-13 18:12:36 +0900

    homework2 - 진행중

--- public/js/controllers/bookList.js
+++ public/js/controllers/bookList.js
@@ -1,10 +1,18 @@
 angular.module('book') //이미 만들어진 모듈을 사용할때에는  ,[]를 만들지 않음
 .controller('bookListCtrl',function($scope, $filter){//$filter : angular가 쓰는 내부 필더
 	var selectedGrade = null;
+	var selectedItem = null;
 	$scope.selectGrade = function(newGrade){
 		selectedGrade =newGrade;
+		selectedItem = newGrade;
+	}
+	$scope.isActive = function(newGrade){
+		return selectedItem ===newGrade;
 	}
 	$scope.gradeFilterFn = function(book){
 		return selectedGrade == null || parseInt(book.grade) == selectedGrade;
+	}
+	$scope.alertInfoFn=function(book){
+		alert('category : '+book.category+'\ngrade : '+book.grade+'\nname : '+book.name + '\nprice : '+book.price+'  \nid : '+book.id);
 	}
 })
