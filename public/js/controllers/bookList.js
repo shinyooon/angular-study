8693af35e6a8a2d3c7dc01b91e8d36088146a133
shinyooon <talkyfull@gmail.com>
2016-01-28 17:51:58 +0900

    homework2 - 완성

--- public/js/controllers/bookList.js
+++ public/js/controllers/bookList.js
@@ -1,18 +1,37 @@
 angular.module('book') //이미 만들어진 모듈을 사용할때에는  ,[]를 만들지 않음
-.controller('bookListCtrl',function($scope, $filter){//$filter : angular가 쓰는 내부 필더
+.constant('bookListActiveClass', 'btn-success')
+.constant('bookListPageCount', 2)
+.controller('bookListCtrl',function($scope, $filter, bookListActiveClass, bookListPageCount){//$filter : angular가 쓰는 내부 필더
 	var selectedGrade = null;
-	var selectedItem = null;
+	$scope.selectedPage = 1;
+	$scope.pageSize = bookListPageCount;
 	$scope.selectGrade = function(newGrade){
 		selectedGrade =newGrade;
 		selectedItem = newGrade;
+		$scope.selectedPage = 1;
 	}
-	$scope.isActive = function(newGrade){
-		return selectedItem ===newGrade;
+	 $scope.selectPage = function(newPage){
+		$scope.selectedPage = newPage;
+	};
+	$scope.getGradeClass = function(grade){
+		return selectedGrade == grade? bookListActiveClass : '';
+	};
+	$scope.getPageClass = function(page){
+		return $scope.selectedPage == page? bookListActiveClass : '';
 	}
 	$scope.gradeFilterFn = function(book){
 		return selectedGrade == null || parseInt(book.grade) == selectedGrade;
 	}
-	$scope.alertInfoFn=function(book){
-		alert('category : '+book.category+'\ngrade : '+book.grade+'\nname : '+book.name + '\nprice : '+book.price+'  \nid : '+book.id);
+	// $scope.alertInfoFn=function(book){
+	// 	alert('category : '+book.category+'\ngrade : '+book.grade+'\nname : '+book.name + '\nprice : '+book.price+'  \nid : '+book.id);
+	// }
+	$scope.alertInfoFn = function(book){
+		var msg = '';
+		for(prop in book){
+			if(prop != '$$hashKey'){
+				msg += prop + ' : ' + book[prop] + '\n';
+			}
+		}
+		alert(msg);
 	}
 })
