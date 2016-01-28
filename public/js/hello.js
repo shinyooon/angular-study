003a36456f6672a8bd2641e8219bf3c1d8f3a9e1
shinyooon <talkyfull@gmail.com>
2016-01-28 17:20:40 +0900

    [수업] 160123 수업내용 - 진행중

--- public/js/hello.js
+++ public/js/hello.js
@@ -6,7 +6,7 @@
 */
 
 angular.module('hello',[])
-	.controller('HelloController', function($scope, $filter, $http, $timeout){ //$http :  ajax를 사용하기 위해
+	.controller('HelloController', function($scope, $filter, $http, $timeout, $q){ //$http :  ajax를 사용하기 위해
 		$scope.hello = {
 			msg: 'hello'
 		}
@@ -63,5 +63,59 @@ angular.module('hello',[])
 				$scope.info = "다시 시작하려면 refresh 해주세요."
 			})
 		}
+
+		// $scope.promiseTest = function(number){
+		// 	var defer = $q.defer();
+		// 	defer.promise
+		// 		.then(function(){
+		// 			return $timeout(function(){//return 받은 값이 primise이면 앞단계가 끝날 때 까지 기다려줌. return값이  promise가 아니라면 $timeout대신 $http.get.
+		// 				//return는 값을 다음 then이 받을 수 있음
+		// 				alert('asyncA');
+		// 			},3000);
+		// 		})
+		// 		.then(function(){
+		// 			return $timeout(function(){
+		// 				alert('asyncB');
+		// 			},1000)
+		// 		})
+		// 		defer.resolve();
+		// };
+		// $scope.promiseTest();
+
+		$scope.promiseQuiz = function(number){
+			var defer = $q.defer();
+			defer.promise
+				.then(
+					asyncC();
+				)
+				.then(
+					 asyncA();
+				)
+				.then(
+					asyncB();
+				)
+				.then(function(){
+					alert('끝났다!');
+				});
+			defer.resolve();
+
+		};
+		function asyncA() {
+			alert('a시작');
+			return $timeout(function(){
+				alert('asyncA');
+			},3000);
+		};
+		function asyncB() {
+			return $timeout(function(){
+				alert('asyncB');
+			},1000);
+		};
+		function asyncC() {
+			alert('c시작');
+				alert('asyncC');
+			},2000);
+		};
+		$scope.promiseQuiz();
 
 	});
